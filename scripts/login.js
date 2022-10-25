78202cd6562e6b4953d3f790ee128aef07025727
AndrewMurphy <amurf26@outlook.com>
2022-10-25 16:22:11 -0700

    js login page

--- scripts/login.js
+++ scripts/login.js
@@ -1,17 +1,13 @@
-let attempts = 0;
+
+
 let cp = "Test@123";
 let ce = "test@outlook.com";
 
 function check(){
     let email = document.getElementById("CE");
-    let pass = document.getEementById("CP");
+    let pass = document.getElementById("CP");
     if(email.value != ce || pass.value != cp){
-        attempts++;
         email.value = "";
         pass.value = "";
-    }
-    if(attempts === 3){
-        email.disable = true;
-        pass.disable = true;
     }
 }
