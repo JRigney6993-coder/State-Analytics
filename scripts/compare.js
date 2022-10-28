e43a2125e593018cf865204afe3e8bad75a7d3e5
Noah-Santos <nsanto591@west-mec.org>
2022-10-28 14:22:19 -0700

    state info
    Added state info for first 12 states

--- scripts/compare.js
+++ scripts/compare.js
@@ -1,3 +1,136 @@
+let Alabama = {
+    taxRate: 9.24,
+    minWage: 7.25,
+    carInM: 113,
+    carInY: 1356,
+    homeInM:142,
+    homeInY: 1705,
+    healthInM: 567,
+    healthInY: 6804,
+    homeCost: 178649
+}
+let Alaska = {
+    taxRate: 1.76,
+    minWage: 10.34,
+    carInM: 119,
+    carInY: 1433,
+    homeInM:109,
+    homeInY: 1307,
+    healthInM: 599,
+    healthInY: 7188,
+    homeCost: 385000
+}
+let Arizona = {
+    taxRate: 8.40,
+    minWage: 12.80,
+    carInM: 145,
+    carInY: 1743,
+    homeInM:100,
+    homeInY: 1200,
+    healthInM: 503,
+    healthInY: 6036,
+    homeCost: 392000
+}
+let Arkansas = {
+    taxRate: 9.47,
+    minWage: 11.00,
+    carInM: 99,
+    carInY: 1189,
+    homeInM:178.50,
+    homeInY: 2142,
+    healthInM: 456,
+    healthInY: 5472,
+    homeCost: 299900
+}
+let California = {
+    taxRate: 8.82,
+    minWage: 14.00,
+    carInM: 119,
+    carInY: 1429,
+    homeInM:122,
+    homeInY: 1460,
+    healthInM: 417,
+    healthInY: 5004,
+    homeCost: 769405
+}
+let Colorado = {
+    taxRate: 7.77,
+    minWage: 12.56,
+    carInM: 204,
+    carInY: 2448,
+    homeInM:201,
+    homeInY: 2412,
+    healthInM: 383,
+    healthInY: 4596,
+    homeCost: 594500
+}
+let Connecticut = {
+    taxRate: 6.35,
+    minWage: 14.00,
+    carInM: 154,
+    carInY: 1845,
+    homeInM:123,
+    homeInY: 1481,
+    healthInM: 534,
+    healthInY: 6408,
+    homeCost: 326124
+}
+let Delaware = {
+    taxRate: 6.60,
+    minWage: 10.50,
+    carInM: 192,
+    carInY: 2302,
+    homeInM:70,
+    homeInY: 845,
+    healthInM: 555,
+    healthInY: 6660,
+    homeCost: 317985
+}
+let Florida = {
+    taxRate: 7.01,
+    minWage: 11.00,
+    carInM: 230,
+    carInY: 2762,
+    homeInM:177,
+    homeInY: 2122,
+    healthInM: 467,
+    healthInY: 5604,
+    homeCost: 382000
+}
+let Georgia = {
+    taxRate: 7.35,
+    minWage: 7.25,
+    carInM: 191,
+    carInY: 2292,
+    homeInM:114,
+    homeInY: 1373,
+    healthInM: 394,
+    healthInY: 4728,
+    homeCost: 256962
+}
+let Hawaii = {
+    taxRate: 4.44,
+    minWage: 12.00,
+    carInM: 120,
+    carInY: 1440,
+    homeInM:98,
+    homeInY: 1174,
+    healthInM: 491,
+    healthInY: 5892,
+    homeCost: 825000
+}
+let Idaho = {
+    taxRate: 6.02,
+    minWage: 7.25,
+    carInM: 87,
+    carInY: 1045,
+    homeInM:82,
+    homeInY: 982,
+    healthInM: 512,
+    healthInY: 6144,
+    homeCost: 446000
+}
+
 let Pennsylvania = {
     taxRate: 3.07,
     minWage: 7.25,
