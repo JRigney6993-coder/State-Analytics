6fe0e4d4c23e3c1ec7ded7e39ea7aa65adbabe3d
Dearborn42 <amurf26@outlook.com>
2022-10-28 08:45:19 -0700

    my states turned into objects

--- scripts/compare.js
+++ scripts/compare.js
@@ -9,3 +9,137 @@ let Pennsylvania = {
     healthInY: 5292,
     homeCost: 206575
 }
+let RhodeIsland = {
+    taxRate:4.87,
+    minWage:12.25,
+    carInM:153,
+    carInY:1836,
+    homeInM:101.75,
+    homeInY:1221,
+    healthInM:286,
+    healthInY:3432,
+    homeCost:422000
+}
+let SouthCarolina = {
+    taxRate:6,
+    minWage:7.25,
+    carInM:86.25,
+    carInY:1035,
+    homeInM:97,
+    homeInY:1165,
+    healthInM:339,
+    healthInY:4068,
+    homeCost:370000
+}
+let SouthDakota = {
+    taxRate:4.5,
+    minWage:9.95,
+    carInM:127.91,
+    carInY:1535,
+    homeInM:177.17,
+    homeInY:2126,
+    healthInM:671,
+    healthInY:8052,
+    homeCost:239500
+}
+let Tennessee = {
+    taxRate:7,
+    minWage:7.25,
+    carInM:71.25,
+    carInY:855,
+    homeInM:137,
+    homeInY:1644,
+    healthInM:508,
+    healthInY:6096,
+    homeCost:419000
+}
+let Texas = {
+    taxRate:8.20,
+    minWage:7.25,
+    carInM:110,
+    carInY:1316,
+    homeInM:345,
+    homeInY:4142,
+    healthInM:424,
+    healthInY:5088,
+    homeCost:390600
+}
+let Utah = {
+    taxRate:4.85,
+    minWage:7.25,
+    carInM:120,
+    carInY:1440,
+    homeInM:55.67,
+    homeInY:668,
+    healthInM:445,
+    healthInY:5340,
+    homeCost:500000
+}
+let Vermont = {
+    taxRate:6.24,
+    minWage:12.55,
+    carInM:95.42,
+    carInY:1145,
+    homeInM:57.17,
+    homeInY:686,
+    healthInM:670.50,
+    healthInY:8046,
+    homeCost:295000
+}
+let Virginia = {
+    taxRate:5.3,
+    minWage:11.00,
+    carInM:184,
+    carInY:2208,
+    homeInM:77,
+    homeInY:924,
+    healthInM:441,
+    healthInY:5292,
+    homeCost:401082
+}
+let Wahsington = {
+    taxRate:9.29,
+    minWage:14.49,
+    carInM:68.29,
+    carInY:819.5,
+    homeInM:102.83,
+    homeInY:1234,
+    healthInM:443,
+    healthInY:5316,
+    homeCost:599626
+}
+let WestVirginia = {
+    taxRate:6,
+    minWage:8.75,
+    carInM:81.54,
+    carInY:978.5,
+    homeInM:93.67,
+    homeInY:1124,
+    healthInM:723,
+    healthInY:8676,
+    homeCost:310000
+}
+let Wisconsin = {
+    taxRate:5.5,
+    minWage:7.25,
+    carInM:80,
+    carInY:963,
+    homeInM:77.33,
+    homeInY:928,
+    healthInM:822,
+    healthInY:9864,
+    homeCost:250000
+}
+let Wyoming = {
+    taxRate:4,
+    minWage:7.25,
+    carInM:128.42,
+    carInY:1541,
+    homeInM:67.08,
+    homeInY:805,
+    healthInM:618,
+    healthInY:7416,
+    homeCost:292509
+}
+
+
