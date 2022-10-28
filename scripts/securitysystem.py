7e3152767025c61a546c4b32e2df6931822906c0
JacobRigney <JRigney6993@gmail.com>
2022-10-28 16:04:03 -0700

    bye bye backend files

--- scripts/securitysystem.py
+++
@@ -1,7 +0,0 @@
-from cryptography.fernet import Fernet
-
-key = open("scripts/data-management/data/pass.key", "rb").read()
-slogan = "Hello!! Welcome to AIM!!".encode()
-a = Fernet(key)
-coded_slogan = a.encrypt(slogan)
-print(coded_slogan)
