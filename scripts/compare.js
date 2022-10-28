e6f340c1741e59d8f0b71150270f6239e70fb8e4
Dearborn42 <amurf26@outlook.com>
2022-10-28 08:16:17 -0700

    Update compare.js

---
+++ scripts/compare.js
@@ -0,0 +1,11 @@
+let Pennsylvania = {
+    taxRate: 3.07,
+    minWage: 7.25,
+    carInM: 51,
+    carInY: 611,
+    homeInM:87,
+    homeInY: 1044,
+    healthInM: 441,
+    healthInY: 5292,
+    homeCost: 206575
+}
